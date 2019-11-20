617f05b8eb18b1f3f4db396fe2d02970a6c556d7
icetea2001 <isac.thoresson.school@gmail.com>
2019-11-20 12:20:27 +0100

    lektion onsdag

--- homework v.39 flagga/JavaFXbutton.java
+++ homework v.39 flagga/JavaFXbutton.java
@@ -31,7 +31,10 @@ public class JavaFXbutton extends Application {
 		HBox top2 = new HBox();
 		top2.getChildren().addAll(tf, bc);
 
-		Button[] buttons1 = { new Button("1"), new Button("2"), new Button("3") };
+		Button[] buttons1 = { 
+				new Button("1"),
+				new Button("2"), 
+				new Button("3") };
 		for (int i = 0; i < buttons1.length; i++) {
 			buttons1[i].setPrefSize(45, 35);
 			knappEvent(buttons1[i]);
@@ -40,7 +43,10 @@ public class JavaFXbutton extends Application {
 		HBox top = new HBox();
 		top.getChildren().addAll(buttons1);
 
-		Button[] buttons2 = { new Button("4"), new Button("5"), new Button("6"), };
+		Button[] buttons2 = { 
+				new Button("4"), 
+				new Button("5"), 
+				new Button("6"), };
 		for (int i = 0; i < buttons2.length; i++) {
 			buttons2[i].setPrefSize(45, 35);
 			knappEvent(buttons2[i]);
@@ -49,7 +55,10 @@ public class JavaFXbutton extends Application {
 		HBox mid = new HBox();
 		mid.getChildren().addAll(buttons2);
 
-		Button[] buttons3 = { new Button("7"), new Button("8"), new Button("9"), };
+		Button[] buttons3 = { 
+				new Button("7"), 
+				new Button("8"), 
+				new Button("9"), };
 		for (int i = 0; i < buttons3.length; i++) {
 			buttons3[i].setPrefSize(45, 35);
 			knappEvent(buttons3[i]);
@@ -58,7 +67,10 @@ public class JavaFXbutton extends Application {
 		HBox bot = new HBox();
 		bot.getChildren().addAll(buttons3);
 
-		Button[] buttons4 = { new Button(","), new Button("0"), new Button("="), };
+		Button[] buttons4 = { 
+				new Button(","),
+				new Button("0"), 
+				new Button("="), };
 		for (int i = 0; i < buttons4.length; i++) {
 			buttons4[i].setPrefSize(45, 35);
 			knappEvent(buttons4[i]);
@@ -67,12 +79,16 @@ public class JavaFXbutton extends Application {
 		HBox bot2 = new HBox();
 		bot2.getChildren().addAll(buttons4);
 
-		Button[] buttons5 = { new Button("x"), new Button("+"), new Button("-"), new Button("/") };
+		Button[] buttons5 = { 
+				new Button("x"), 
+				new Button("+"), 
+				new Button("-"), 
+				new Button("/") };
 		for (int i = 0; i < buttons5.length; i++) {
 			buttons5[i].setPrefSize(45, 35);
 			knappEvent(buttons5[i]);
 		}
-
+		
 		VBox sidh = new VBox();
 		sidh.getChildren().addAll(buttons5);
 
@@ -87,7 +103,7 @@ public class JavaFXbutton extends Application {
 		primaryStage.show();
 
 	}
-
+	
 	public void knappEvent(Button knp) {
 		if (Character.isDigit(knp.getText().charAt(0))) {
 			knp.setOnAction(event -> {
@@ -95,7 +111,8 @@ public class JavaFXbutton extends Application {
 				nmrOrdning += knp.getText();
 				tf.setText(nmrOrdning);
 			});
-		} else if (knp.getText().equals(",")) {
+		} 
+		else if (knp.getText().equals(",")) {
 			knp.setOnAction(event -> {
 				boolean kma = false;
 				for (int i = 0; i < nummer.length(); i++) {
@@ -109,22 +126,72 @@ public class JavaFXbutton extends Application {
 					tf.setText(nmrOrdning);
 				}
 			});
-		}else if(knp.getText().equals("clear")) {
+		}
+		else if(knp.getText().equals("clear")) {
 			knp.setOnAction(event -> {
 				nummer = "";
 				nmrOrdning = "";
+				talOrdning.clear();
 				tf.setText(nmrOrdning);
 			});
-		}else if(knp.getText().equals("+") || knp.getText().equals("-") || knp.getText().equals("x") || knp.getText().equals("/")) {
+		}
+		else if(knp.getText().equals("+") || 
+				knp.getText().equals("-") || 
+				knp.getText().equals("x") || 
+				knp.getText().equals("/")) {
 			knp.setOnAction(event -> {
-				talOrdning.add(nummer);
+				talOrdning.add(nummer.replace(',', '.'));
 				talOrdning.add(knp.getText());
 				nummer = "";
 				nmrOrdning += (" " + knp.getText() + " ");
 				tf.setText(nmrOrdning);
 			});
 		}
+		else if(knp.getText().equals("=")) {
+			knp.setOnAction(event -> {
+				talOrdning.add(nummer.replace(',', '.'));
+				nummer = "";
+				double sum = 0;
+				for (int i = 0; i < talOrdning.size(); i++) {
+					if(i == 0) {
+						sum = Double.parseDouble(talOrdning.get(i));
+					}
+					if(isNummer(talOrdning.get(i)) && i != 0) {
+						sum = calc(sum, Double.parseDouble(talOrdning.get(i)), talOrdning.get(i - 1));
+					}
+				}
+				talOrdning.clear();
+				nummer = Double.toString(sum);
+				nmrOrdning = Double.toString(sum);
+				tf.setText(nmrOrdning);
+			});
+		}
+	}
+	
+	public boolean isNummer(String s) {
+		try {
+			double d = Double.parseDouble(s);
+		}
+		catch (NumberFormatException | NullPointerException nfe) {
+			return false;
+		}
+		return true;
+	}
+	
+	public double calc(double num1, double num2 , String rknst) {
+		if(rknst.equals("+")) {
+			return num1 + num2;
+		}
+		else if(rknst.equals("-")) {
+			return num1 - num2;
+		}
+		else if(rknst.equals("x")) {
+			return num1 * num2;
+		}
+		else if(rknst.equals("/")) {
+			return num1 / num2;
+		}
+		return 0;
 	}
 
 }
-
